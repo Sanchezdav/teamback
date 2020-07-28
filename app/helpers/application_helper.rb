aaf67535e451ca45553d3107715554da1deaecf7
David <sanchez.dav90@gmail.com>
2020-07-27 22:51:43 -0500

    Add Bulma to devise views and navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def alert_class_for(flash_type)
+    {
+      success: "is-primary",
+      error: "is-danger",
+      alert: "is-warning",
+      notice: "is-info"
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def show_input_error(resource, attribute)
+    return unless resource.errors.has_key?(attribute)
+
+    resource.errors.full_messages_for(attribute).join(', ')
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,15 @@ require("channels")
 
 import '../stylesheets/application'
 
+document.addEventListener('turbolinks:load', () => {
+  let alert = document.querySelector('.alert')
+
+  if (alert) {
+    setTimeout(function () {
+      alert.classList.add('hidden')
+    }, 2000)
+  }
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
